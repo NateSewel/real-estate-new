2e2fc0cd5d6027c6ed2cf5c7e8106f7162edaa32
Nate247 <nathanielisewede@gmail.com>
2025-01-31 12:12:57 +0000

    Backend error fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,7 @@ export const createUser = asyncHandler(async (req, res) => {
   let { email } = req.body;
 
   //   Check if User exists
-  const userExists = await prisma.user.findUnique({ where: { email } });
+  const userExists = await prisma.user.findUnique({ where: { email: email } });
 
   if (!userExists) {
     const user = await prisma.user.create({ data: req.body });
@@ -14,7 +14,7 @@ export const createUser = asyncHandler(async (req, res) => {
       message: "User registered successfully!",
       user: user,
     });
-  } else res.status(201).json({ message: "User already registered!" });
+  } else res.status(201).send({ message: "User already registered!" });
 });
 
 // Function to Booking a visit to residency
